02a70a67e16e135d2720c0fe519dc3ddce5d6f64
mamoon123 <moonasus558@gmail.com>
2021-03-28 11:37:40 +0530

    1.1 with blur folder

---
+++ utility/src/main/java/com/moon/utility/blur/Helper.java
@@ -0,0 +1,20 @@
+package com.moon.utility.blur;
+
+import android.view.View;
+import android.view.animation.AlphaAnimation;
+public class Helper {
+    public static boolean hasZero(int... args) {
+        for (int num : args) {
+            if (num == 0) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public static void animate(View v, int duration) {
+        AlphaAnimation alpha = new AlphaAnimation(0f, 1f);
+        alpha.setDuration(duration);
+        v.startAnimation(alpha);
+    }
+}
